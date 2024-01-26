5b30df52f7f7fbbb38a996688cea9eaf4e3a9ffa
Will Terry <wterry1999@gmail.com>
2024-01-26 10:19:43 -0500

    Updated a bit small changes

--- Basic.java
+++ Basic.java
@@ -2,13 +2,13 @@ public class Basic {
     public static void main(String[] args) throws Exception {
         //Main must ensure there's only one argument.
         //If more than 1, print error message and exit.
-        if (args.length != 1) {
+        if (args.length != 0) {
             //Error message
             throw new Exception("Error: more than one argument!");
         }
         else {
             //RUN CODE
-            System.out.println("Hello World");
+            System.out.println("Hello World32");
         }
      }
 }
---
+++ Token.java
@@ -0,0 +1,10 @@
+public class Token {
+
+    //Enum to hold the token type
+    public enum TokenType {
+        WORD, NUMBER, ENDOFLINE
+    }
+    public String tokenValue() {
+        return tokenValue();
+    }
+}
