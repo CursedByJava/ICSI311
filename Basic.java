dd13224d6580ae8360d597972e86558d07038692
Will Terry <wterry1999@gmail.com>
2024-02-27 09:52:08 -0500

    Updated a bit small changes

--- Basic.java
+++ Basic.java
@@ -5,16 +5,16 @@ public class Basic {
 
         //Main must ensure there's only one argument.
         //If more than 1, print error message and exit.
-        if (args.length != 1) {
-            //Error message
-            throw new Exception("Error: more than one argument: " + args);
-        }
-        else {
-            //sets arguments to the filename
-            arguments = args[0];
-            //RUN CODE
-            Lexer lexer = new Lexer();
-            lexer.lex(arguments);
-        }
+//        if (args.length != 1) {
+//            //Error message
+//            throw new Exception("Error: more than one argument: " + args);
+//        }
+//        else {
+//            //sets arguments to the filename
+//            arguments = args[0];
+//            //RUN CODE
+//            Lexer lexer = new Lexer();
+//            lexer.lex(arguments);
+//        }
      }
 }
